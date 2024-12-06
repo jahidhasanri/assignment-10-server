ace1d7cd2434feb8bb623ef2ed783b244869d904
jahid hasan rifat <jahadhasanrifat@gmail.com>
2024-12-06 16:57:33 +0600

    add delete method

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
           res.send(result);  
       
   });
+
+  app.delete('/equepment/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id: new ObjectId(id)}
+    const result=databasecollection.deleteOne(query)
+    res.res(result)
+  })
   
   
   
